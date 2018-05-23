6481534c845dce902618ccd5033bcc7ba8695c13
gabderakhmanov <gabderakhmanov@gmail.com>
2018-05-23 13:53:39 +0300

    add security

---
+++ src/main/java/com/abg/max/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.abg.max.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
--- src/main/java/com/abg/max/config/WebConfig.java
+++ src/main/java/com/abg/max/config/WebConfig.java
@@ -1,22 +1,32 @@
 package com.abg.max.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.Configuration;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
 
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.abg.max")
+@PropertySource("classpath:persistence-mysql.properties")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    // set up variable to hold the properties
+    @Autowired
+    private Environment env;
+
+    // set up a logger for diagnostic
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -36,5 +46,44 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
+    // define a bean for security datasource
+    @Bean
+    public DataSource securityDataSource(){
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set jdbc driver
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        // log the connection props
+        logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    // helper to convert from string to int
+    private int getIntProperty(String propName){
+        String propVal = env.getProperty(propName);
+        int intPropVal = Integer.parseInt(propVal);
+        return intPropVal;
+
+    }
 
 }
