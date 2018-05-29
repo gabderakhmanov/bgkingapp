9fe2453edc4bd3eb4a27f41b07776c20e902c351
gabderakhmanov <gabderakhmanov@gmail.com>
2018-05-29 13:06:31 +0300

    add pathVariable to get one item (student)

--- src/main/java/com/abg/max/rest/StudentRestController.java
+++ src/main/java/com/abg/max/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.abg.max.rest;
 
 import com.abg.max.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +14,29 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("John", "Cool"));
+        theStudents.add(new Student("Mary", "Lee"));
+        theStudents.add(new Student("Tom", "Sailor"));
+    }
+
 
     // "/students" endpoint
     @GetMapping("/students")
     public List<Student> getStudents() {
-
-        List<Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("John", "Cool"));
-        theStudents.add(new Student("Mary", "Lee"));
-        theStudents.add(new Student("Tom", "Sailor"));
-
         return theStudents;
+    }
+
+    // "/students/{studentId}" endpoint
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
     }
 
 }
